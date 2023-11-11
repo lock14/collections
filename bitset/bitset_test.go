1c6b91ae179fe23e8428efb6cd05a00c273ae8b3
Brian Bechtel <brianbechtel@outlook.com>
2023-11-10 22:40:02 -0800

    add test

--- bitset/bitset_test.go
+++ bitset/bitset_test.go
@@ -4,6 +4,16 @@ import (
 	"testing"
 )
 
+func TestAllBitsIntializedToZero(t *testing.T) {
+	n := 128
+	bitSet := New(NumBits(uint32(n)))
+	for i := 0; i < n; i++ {
+		if bitSet.Get(uint32(i)) {
+			t.Fatalf("excepted bit %d to be unset, but it was not", i)
+		}
+	}
+}
+
 func TestSetBit(t *testing.T) {
 	n := 128
 	bitSet := New(NumBits(uint32(n)))
