e0ce1c156d7b0ec3bf558f6246963d4ac248a58d
Brian Bechtel <brianbechtel@outlook.com>
2024-02-15 23:56:01 -0800

    move iterable definition to collections package

--- iterator/iterator.go
+++ iterator/iterator.go
@@ -11,23 +11,6 @@ type Iterator[T any] interface {
 	Next() (T, error)
 }
 
-// Iterable denotes a type that can be iterated over
-// by using the Iterator supplied using the Iterator method.
-type Iterable[T any] interface {
-	// Iterator returns an Iterator over the elements of this Iterable.
-	Iterator() Iterator[T]
-	// Elements returns a channel containing the elements of this Iterable.
-	// The channel provided will be closed automatically after all elements
-	// have been read from the channel. If all elements from the channel
-	// are not read, then the channel will not be closed. This method is
-	// intended to be used primarily with the for...range construct.
-	//
-	//   for e := range i.Elements() {
-	//      // do something with e
-	//   }
-	Elements() chan T
-}
-
 // Elements produces a channel that contains the elements of the iterator.
 // The channel provided will be closed automatically after all elements
 // have been read from the channel. If all elements from the channel
