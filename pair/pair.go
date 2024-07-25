c351e5573eaa8ea61063981e075cd985b8dd0540
Brian Bechtel <brianbechtel@outlook.com>
2024-07-25 00:59:29 -0700

    update pair

--- pair/pair.go
+++ pair/pair.go
@@ -14,12 +14,12 @@ func New[T1 any, T2 any](t1 T1, t2 T2) *Pair[T1, T2] {
 	}
 }
 
-func (p *Pair[T1, T2]) Fst() *T1 {
-	return &p.fst
+func (p *Pair[T1, T2]) Fst() T1 {
+	return p.fst
 }
 
-func (p *Pair[T1, T2]) Snd() *T2 {
-	return &p.snd
+func (p *Pair[T1, T2]) Snd() T2 {
+	return p.snd
 }
 
 func (p *Pair[T1, T2]) UnWrap() (T1, T2) {
