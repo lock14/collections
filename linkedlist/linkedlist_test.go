22a0c339285042f018b011f30860f5c0af798582
Brian Bechtel <brianbechtel@outlook.com>
2024-08-08 23:36:29 -0700

    Make LinkedList a Deque

--- linkedlist/linkedlist_test.go
+++ linkedlist/linkedlist_test.go
@@ -1,1 +1,13 @@
 package linked_list
+
+import (
+	"github.com/lock14/collections"
+	"testing"
+)
+
+func TestType(t *testing.T) {
+	l := New[int]()
+	testType[int](l)
+}
+
+func testType[T any](deque collections.Deque[T]) {}
