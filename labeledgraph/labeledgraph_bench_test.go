dab7e19ecda14fb912f8443d5122f414b01eb449
Brian Bechtel <brianrbechtel@gmail.com>
2026-06-20 19:21:45 -0700

    test(labeledgraph): add benchmark suite

---
+++ labeledgraph/labeledgraph_bench_test.go
@@ -0,0 +1,88 @@
+package labeledgraph_test
+
+import (
+	"github.com/lock14/collections/labeledgraph"
+	"testing"
+)
+
+func BenchmarkLabeledGraph_AddVertex(b *testing.B) {
+	g := labeledgraph.New[int, string]()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		g.AddVertex(i)
+	}
+}
+
+func BenchmarkLabeledGraph_AddEdge_Directed(b *testing.B) {
+	g := labeledgraph.New[int, string](labeledgraph.Directed())
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		g.AddEdge(i, i+1, "label")
+	}
+}
+
+func BenchmarkLabeledGraph_AddEdge_Undirected(b *testing.B) {
+	g := labeledgraph.New[int, string]()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		g.AddEdge(i, i+1, "label")
+	}
+}
+
+func BenchmarkLabeledGraph_RemoveVertex(b *testing.B) {
+	g := labeledgraph.New[int, string]()
+	for i := 0; i < b.N; i++ {
+		g.AddVertex(i)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		g.RemoveVertex(i)
+	}
+}
+
+func BenchmarkLabeledGraph_RemoveEdge(b *testing.B) {
+	g := labeledgraph.New[int, string]()
+	for i := 0; i < b.N; i++ {
+		g.AddEdge(i, i+1, "l")
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		g.RemoveEdge(i, i+1)
+	}
+}
+
+func BenchmarkLabeledGraph_IterateVertices(b *testing.B) {
+	g := labeledgraph.New[int, string]()
+	for i := 0; i < 1000; i++ {
+		g.AddVertex(i)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _ = range g.Vertices() {
+		}
+	}
+}
+
+func BenchmarkLabeledGraph_IterateEdges(b *testing.B) {
+	g := labeledgraph.New[int, string]()
+	for i := 0; i < 1000; i++ {
+		g.AddEdge(i, i+1, "l")
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, _ = range g.Edges() {
+		}
+	}
+}
+
+func BenchmarkLabeledGraph_IncidentEdges(b *testing.B) {
+	g := labeledgraph.New[int, string]()
+	for i := 0; i < 1000; i++ {
+		g.AddEdge(0, i, "l")
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, _ = range g.IncidentEdges(0) {
+		}
+	}
+}
