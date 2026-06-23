eef7441f8247d94d84dfb0d983b9f0e9e8dbd8d9
Brian Bechtel <brianrbechtel@gmail.com>
2026-06-22 21:23:27 -0700

    refactor(hashmap): remove custom implementation in favor of built-in wrapper

---
+++ hashmap/hashmap_bench_test.go
@@ -0,0 +1,56 @@
+package hashmap_test
+
+import (
+	"github.com/lock14/collections/hashmap"
+	"testing"
+)
+
+func BenchmarkHashMap_Put(b *testing.B) {
+	hm := hashmap.New[int, int]()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		hm.Put(i, i)
+	}
+}
+
+func BenchmarkHashMap_Put_Preallocated(b *testing.B) {
+	hm := hashmap.New[int, int](hashmap.WithCapacity(b.N))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		hm.Put(i, i)
+	}
+}
+
+func BenchmarkHashMap_Get(b *testing.B) {
+	hm := hashmap.New[int, int]()
+	for i := 0; i < 1000; i++ {
+		hm.Put(i, i)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		hm.Get(i % 1000)
+	}
+}
+
+func BenchmarkHashMap_Remove(b *testing.B) {
+	hm := hashmap.New[int, int]()
+	for i := 0; i < b.N; i++ {
+		hm.Put(i, i)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		hm.Remove(i)
+	}
+}
+
+func BenchmarkHashMap_IterateAll(b *testing.B) {
+	hm := hashmap.New[int, int]()
+	for i := 0; i < 1000; i++ {
+		hm.Put(i, i)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, _ = range hm.All() {
+		}
+	}
+}
--- hashmap/hashmap_test.go
+++ hashmap/hashmap_test.go
@@ -1,313 +1,135 @@
 package hashmap
 
 import (
+	"slices"
+	"sort"
 	"testing"
 )
 
-type Foo struct {
-	bar int
-}
-
-type Person struct {
-	Name string
-	age  int
-	Foo  Foo
-}
-
-func GetPutAndRemoveSameAsBuiltInTestCase[K comparable, V comparable](entries map[K]V) func(t *testing.T) {
-	return func(t *testing.T) {
-		t.Parallel()
-		builtIn := make(map[K]V)
-		hashMap := New[K, V]()
-
-		for k, v := range entries {
-			builtIn[k] = v
-			hashMap.Put(k, v)
-			if got, want := hashMap.Size(), len(builtIn); got != want {
-				t.Errorf("unexpected number of entries: got %d, want %d", got, want)
-			}
-			biEntries := make([]struct {
-				k K
-				v V
-			}, 0, len(builtIn))
-			for k, v := range builtIn {
-				biEntries = append(biEntries, struct {
-					k K
-					v V
-				}{k: k, v: v})
-			}
-			hmEntries := make([]struct {
-				k K
-				v V
-			}, 0, len(builtIn))
-			for k, v := range hashMap.All() {
-				hmEntries = append(hmEntries, struct {
-					k K
-					v V
-				}{k: k, v: v})
-			}
-			if got, want := len(hmEntries), len(biEntries); got != want {
-				t.Errorf("unexpected number of entries: got %d, want %d", got, want)
-			}
-		}
-		if got, want := hashMap.Size(), len(builtIn); got != want {
-			t.Errorf("unexpected number of entries: got %d, want %d", got, want)
-		}
-		for k := range entries {
-			gotV, gotOk := hashMap.Get(k)
-			wantV, wantOK := builtIn[k]
-			if gotOk != wantOK {
-				t.Errorf("got %v, want %v", gotOk, wantOK)
-			}
-			if gotV != wantV {
-				t.Errorf("got %v, want %v", gotV, wantV)
-			}
-			if got, want := hashMap.Size(), len(builtIn); got != want {
-				t.Errorf("unexpected number of entries: got %d, want %d", got, want)
-			}
-			biEntries := make([]struct {
-				k K
-				v V
-			}, 0, len(builtIn))
-			for k, v := range builtIn {
-				biEntries = append(biEntries, struct {
-					k K
-					v V
-				}{k: k, v: v})
-			}
-			hmEntries := make([]struct {
-				k K
-				v V
-			}, 0, hashMap.Size())
-			for k, v := range hashMap.All() {
-				hmEntries = append(hmEntries, struct {
-					k K
-					v V
-				}{k: k, v: v})
-			}
-			if got, want := len(hmEntries), len(biEntries); got != want {
-				t.Errorf("unexpected number of entries: got %d, want %d", got, want)
-			}
-		}
-		if got, want := hashMap.Size(), len(builtIn); got != want {
-			t.Errorf("unexpected number of entries: got %d, want %d", got, want)
-		}
-		for k := range entries {
-			delete(builtIn, k)
-			hashMap.Remove(k)
-			if got, want := hashMap.Size(), len(builtIn); got != want {
-				t.Errorf("unexpected number of entries: got %d, want %d", got, want)
-			}
-			biEntries := make([]struct {
-				k K
-				v V
-			}, 0, len(builtIn))
-			for k, v := range builtIn {
-				biEntries = append(biEntries, struct {
-					k K
-					v V
-				}{k: k, v: v})
-			}
-			hmEntries := make([]struct {
-				k K
-				v V
-			}, 0, hashMap.Size())
-			for k, v := range hashMap.All() {
-				hmEntries = append(hmEntries, struct {
-					k K
-					v V
-				}{k: k, v: v})
-			}
-			if got, want := len(hmEntries), len(biEntries); got != want {
-				t.Errorf("unexpected number of entries: got %d, want %d", got, want)
-			}
-		}
-		if got, want := hashMap.Size(), len(builtIn); got != want {
-			t.Errorf("unexpected number of entries: got %d, want %d", got, want)
-		}
-	}
-}
-
-func TestGetPutAndRemoveSameAsBuiltInMapInt(t *testing.T) {
+func TestNew(t *testing.T) {
 	t.Parallel()
 	cases := []struct {
-		name    string
-		entries map[int]int
+		name  string
+		opts  []Option
+		check func(*testing.T, *HashMap[int, string])
 	}{
 		{
-			name:    "empty",
-			entries: map[int]int{},
+			name: "default",
+			check: func(t *testing.T, hm *HashMap[int, string]) {
+				if hm.Size() != 0 || !hm.Empty() {
+					t.Errorf("expected empty map")
+				}
+			},
 		},
 		{
-			name:    "one",
-			entries: map[int]int{45: 2},
-		},
-		{
-			name: "many",
-			entries: map[int]int{
-				531279: 235585,
-				774428: 293791,
-				508129: 869760,
-				867759: 339288,
-				517856: 441415,
-				203188: 565133,
-				109294: 270937,
-				392301: 97964,
-				933428: 183517,
-				107478: 721549,
+			name: "capacity",
+			opts: []Option{WithCapacity(100)},
+			check: func(t *testing.T, hm *HashMap[int, string]) {
+				hm.Put(1, "a")
+				if hm.Size() != 1 {
+					t.Errorf("expected size 1")
+				}
 			},
 		},
 	}
-
 	for _, tc := range cases {
-		t.Run(tc.name, GetPutAndRemoveSameAsBuiltInTestCase(tc.entries))
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			hm := New[int, string](tc.opts...)
+			tc.check(t, hm)
+		})
 	}
 }
 
-func TestGetPutAndRemoveSameAsBuiltInMapFloat(t *testing.T) {
+func TestWrap(t *testing.T) {
 	t.Parallel()
-	cases := []struct {
-		name    string
-		entries map[float64]float64
-	}{
-		{
-			name:    "empty",
-			entries: map[float64]float64{},
-		},
-		{
-			name:    "one",
-			entries: map[float64]float64{45: 2},
-		},
-		{
-			name: "many",
-			entries: map[float64]float64{
-				531279: 235585,
-				774428: 293791,
-				508129: 869760,
-				867759: 339288,
-				517856: 441415,
-				203188: 565133,
-				109294: 270937,
-				392301: 97964,
-				933428: 183517,
-				107478: 721549,
-			},
-		},
+	m := map[int]string{1: "a", 2: "b"}
+	hm := Wrap(m)
+	if hm.Size() != 2 {
+		t.Errorf("expected size 2")
 	}
-
-	for _, tc := range cases {
-		t.Run(tc.name, GetPutAndRemoveSameAsBuiltInTestCase(tc.entries))
+	if v, _ := hm.Get(1); v != "a" {
+		t.Errorf("expected 'a'")
 	}
 }
 
-func TestGetPutAndRemoveSameAsBuiltInMapString(t *testing.T) {
+func TestHashMap_Operations(t *testing.T) {
 	t.Parallel()
 	cases := []struct {
-		name    string
-		entries map[string]string
+		name  string
+		check func(*testing.T, *HashMap[int, string])
 	}{
 		{
-			name:    "empty",
-			entries: map[string]string{},
+			name: "put_get_contains",
+			check: func(t *testing.T, hm *HashMap[int, string]) {
+				hm.Put(1, "a")
+				hm.Put(2, "b")
+				hm.Put(1, "c") // overwrite
+				
+				if hm.Size() != 2 {
+					t.Errorf("expected size 2")
+				}
+				if v, ok := hm.Get(1); !ok || v != "c" {
+					t.Errorf("expected 'c', got %v", v)
+				}
+				if hm.ContainsKey(3) {
+					t.Errorf("did not expect 3")
+				}
+				if !hm.ContainsKey(2) {
+					t.Errorf("expected 2")
+				}
+			},
 		},
 		{
-			name:    "one",
-			entries: map[string]string{"45": "2"},
+			name: "remove",
+			check: func(t *testing.T, hm *HashMap[int, string]) {
+				hm.Put(1, "a")
+				hm.Remove(1)
+				hm.Remove(2) // non-existent
+				
+				if !hm.Empty() {
+					t.Errorf("expected empty")
+				}
+			},
 		},
 		{
-			name: "many",
-			entries: map[string]string{
-				"531279": "235585",
-				"774428": "293791",
-				"508129": "869760",
-				"867759": "339288",
-				"517856": "441415",
-				"203188": "565133",
-				"109294": "270937",
-				"392301": "97964",
-				"933428": "183517",
-				"107478": "721549",
+			name: "clear",
+			check: func(t *testing.T, hm *HashMap[int, string]) {
+				hm.Put(1, "a")
+				hm.Clear()
+				
+				if hm.Size() != 0 {
+					t.Errorf("expected empty")
+				}
+			},
+		},
+		{
+			name: "iterators",
+			check: func(t *testing.T, hm *HashMap[int, string]) {
+				hm.Put(1, "a")
+				hm.Put(2, "b")
+				
+				keys := slices.Collect(hm.Keys())
+				sort.Ints(keys)
+				if !slices.Equal(keys, []int{1, 2}) {
+					t.Errorf("keys wrong")
+				}
+				
+				vals := slices.Collect(hm.Values())
+				sort.Strings(vals)
+				if !slices.Equal(vals, []string{"a", "b"}) {
+					t.Errorf("values wrong")
+				}
 			},
 		},
 	}
-
 	for _, tc := range cases {
-		t.Run(tc.name, GetPutAndRemoveSameAsBuiltInTestCase(tc.entries))
-	}
-}
-
-func TestGetPutAndRemoveSameAsBuiltInMapStruct(t *testing.T) {
-	t.Parallel()
-	cases := []struct {
-		name    string
-		entries map[Person]Person
-	}{
-		{
-			name:    "empty",
-			entries: map[Person]Person{},
-		},
-		{
-			name: "one",
-			entries: map[Person]Person{
-				{Name: "bob", age: 21}: {Name: "mary", age: 19},
-			},
-		},
-		{
-			name: "many",
-			entries: map[Person]Person{
-				{Name: "bob", age: 21}:   {Name: "mary", age: 26},
-				{Name: "andy", age: 45}:  {Name: "rachel", age: 73},
-				{Name: "susan", age: 67}: {Name: "anne", age: 62},
-				{Name: "john", age: 34}:  {Name: "krystal", age: 55},
-				{Name: "lilly", age: 35}: {Name: "penelope", age: 50},
-				{Name: "hank", age: 89}:  {Name: "fred", age: 41},
-				{Name: "mark", age: 12}:  {Name: "rick", age: 44},
-				{Name: "bill", age: 23}:  {Name: "nancy", age: 48},
-				{Name: "ben", age: 48}:   {Name: "colin", age: 26},
-				{Name: "paul", age: 39}:  {Name: "wendy", age: 24},
-			},
-		},
-	}
-
-	for _, tc := range cases {
-		t.Run(tc.name, GetPutAndRemoveSameAsBuiltInTestCase(tc.entries))
-	}
-}
-
-func TestGetPutAndRemoveSameAsBuiltInMapStructPointers(t *testing.T) {
-	t.Parallel()
-	cases := []struct {
-		name    string
-		entries map[*Person]*Person
-	}{
-		{
-			name:    "empty",
-			entries: map[*Person]*Person{},
-		},
-		{
-			name: "one",
-			entries: map[*Person]*Person{
-				{Name: "bob", age: 21}: {Name: "mary", age: 19},
-			},
-		},
-		{
-			name: "many",
-			entries: map[*Person]*Person{
-				{Name: "bob", age: 21}:   {Name: "mary", age: 26},
-				{Name: "andy", age: 45}:  {Name: "rachel", age: 73},
-				{Name: "susan", age: 67}: {Name: "anne", age: 62},
-				{Name: "john", age: 34}:  {Name: "krystal", age: 55},
-				{Name: "lilly", age: 35}: {Name: "penelope", age: 50},
-				{Name: "hank", age: 89}:  {Name: "fred", age: 41},
-				{Name: "mark", age: 12}:  {Name: "rick", age: 44},
-				{Name: "bill", age: 23}:  {Name: "nancy", age: 48},
-				{Name: "ben", age: 48}:   {Name: "colin", age: 26},
-				{Name: "paul", age: 39}:  {Name: "wendy", age: 24},
-			},
-		},
-	}
-
-	for _, tc := range cases {
-		t.Run(tc.name, GetPutAndRemoveSameAsBuiltInTestCase(tc.entries))
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			hm := New[int, string]()
+			tc.check(t, hm)
+		})
 	}
 }
